46b3d02f7490bdee33484b51e0b7d04f073ef5c6
Tashi Jain <tashjain88@gmail.com>
2020-06-18 21:06:14 -0700

    Initial commit

---
+++ app/src/main/java/com/example/timestableapp/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.timestableapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.SeekBar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        //SeekBar seekBar =
+//
+//        ListView myListView = findViewById(R.id.myListView);
+
+
+
+
+    }
+}
