38dd0067d3902eb87c68a7ffe543952cc7732806
Tashi Jain <tashjain88@gmail.com>
2020-06-18 21:25:55 -0700

    set up seek bar functionality

--- app/src/main/java/com/example/timestableapp/MainActivity.java
+++ app/src/main/java/com/example/timestableapp/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.widget.ListView;
 import android.widget.SeekBar;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -13,9 +15,45 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        //SeekBar seekBar =
-//
-//        ListView myListView = findViewById(R.id.myListView);
+        final SeekBar timesTablesSeekBar = findViewById(R.id.timeTablesSeekBar);
+
+        timesTablesSeekBar.setMax(20);
+        timesTablesSeekBar.setProgress(10);
+
+        timesTablesSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                // just so that we have option to change min anytime
+                int min = 1;
+                int timesTableNum;
+
+                if(progress < min) {
+                    timesTableNum = min;
+                    timesTablesSeekBar.setProgress(min);
+                } else {
+                    timesTableNum = progress;
+                }
+
+                ArrayList<String> timesTableContent = new ArrayList<>();
+                ListView timesTablesListView = findViewById(R.id.timesTablesListView);
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+
+
+
+
 
 
 
